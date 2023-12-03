492f44d2a36d79e4be28f09f831937444c972c1c
alwa <alix.warnke@gmail.com>
2023-12-03 23:20:33 +0100

    Day 3: Gear Ratios (part 1)

--- src/main/kotlin/org/example/Main.kt
+++ src/main/kotlin/org/example/Main.kt
@@ -5,7 +5,7 @@ import java.io.File
 
 fun main() {
     val twoPartChallenges: List<TwoPartChallenge<*,*>> =
-        listOf(Day1, Day2)
+        listOf(Day1, Day2, Day3)
     for (i in 1..twoPartChallenges.size) {
         println("=Day $i=")
         val file = File(ClassLoader.getSystemResource("day${i}/input.txt").file)
