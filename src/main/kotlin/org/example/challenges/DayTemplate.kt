5430ebba703346659680bde75ef95c0ecbca2f3c
alixwar <alix.warnke@assaabloy.com>
2024-12-31 09:40:25 +0100

    Added template

---
+++ src/main/kotlin/org/example/challenges/DayTemplate.kt
@@ -0,0 +1,22 @@
+package org.example.challenges
+
+import org.example.TwoPartChallenge
+import java.io.File
+
+object DayTemplate : TwoPartChallenge<Int, Int> {
+
+    override fun part1(file: File): Int {
+        var total = 0
+        file.forEachLine {
+        }
+        return total
+    }
+
+    override fun part2(file: File): Int {
+        var total = 0
+        file.forEachLine {
+        }
+        return total
+    }
+
+}
